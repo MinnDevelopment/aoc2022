7ac9ceac6f5fa993091434cc00fa98209dea8860
Florian Spieß <business@minnced.club>
2022-12-09 15:35:48 +0100

    Cleanup and use hashbrown

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,32 +1,44 @@
-use std::{collections::HashSet, time::Instant};
+use std::time::Instant;
+
+use hashbrown::HashSet;
 
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     let start_time = Instant::now();
+
     let inputs = parse_input();
+    let input_time = Instant::now();
+
     let a = part1(&inputs);
+    let part1_time = Instant::now();
 
     let b = part2(&inputs);
-    let end_time = Instant::now();
+    let part2_time = Instant::now();
 
-    println!("Part 1:");
+    println!("Input Parsing: {:?}", input_time - start_time);
+
+    println!("\nPart 1:");
     println!("Visited: {}", a);
+    println!("Time: {:?}", part1_time - input_time);
+
     println!("\nPart 2:");
     println!("Visited: {}", b);
-    println!("\nTime: {:?}", end_time - start_time);
+    println!("Time: {:?}", part2_time - part1_time);
+
+    println!("\nTotal Time: {:?}", part2_time - start_time);
 }
 
-fn part1(inputs: &[(Move, i32)]) -> usize {
+fn part1(inputs: &[(i32, i32)]) -> usize {
     let mut visited = HashSet::new();
 
-    let mut head = Tail(0, 0);
+    let mut head = Knot(0, 0);
     let mut tail = head;
     visited.insert(tail);
 
-    for &(dir, dist) in inputs.iter() {
-        head.move_head(dir, dist);
-        while tail.move_tail(head) {
+    for &motion in inputs {
+        head.pull(motion);
+        while tail.follow(head) {
             visited.insert(tail);
         }
     }
@@ -34,68 +46,57 @@ fn part1(inputs: &[(Move, i32)]) -> usize {
     visited.len()
 }
 
-fn part2(inputs: &[(Move, i32)]) -> usize {
+fn part2(inputs: &[(i32, i32)]) -> usize {
     const N: usize = 9;
 
     let mut visited = HashSet::new();
-    let mut head = Tail(0, 0);
-    let mut games = [Tail(0, 0); N];
+    let mut head = Knot(0, 0);
+    let mut rope = [Knot(0, 0); N];
 
-    for &(dir, dist) in inputs.iter() {
-        head.move_head(dir, dist);
-        while games[0].move_tail(head) {
-            for i in 1..games.len() {
-                let dest = games[i - 1];
-                games[i].move_tail(dest);
+    for &motion in inputs {
+        head.pull(motion);
+        while rope[0].follow(head) {
+            for i in 1..N {
+                let dest = rope[i - 1];
+                rope[i].follow(dest);
             }
 
-            visited.insert(games[N - 1]);
+            visited.insert(rope[N - 1]);
         }
     }
 
     visited.len()
 }
 
-fn parse_input() -> Vec<(Move, i32)> {
+fn parse_input() -> Box<[(i32, i32)]> {
     INPUT
         .lines()
         .filter_map(|line| line.split_once(' '))
         .map(|(dir, dist)| {
-            let dir = match dir {
-                "U" => Move::Up,
-                "D" => Move::Down,
-                "L" => Move::Left,
-                "R" => Move::Right,
+            let d: i32 = dist.parse().unwrap();
+            match dir {
+                "U" => (0, d),
+                "D" => (0, -d),
+                "L" => (-d, 0),
+                "R" => (d, 0),
                 _ => unreachable!("Invalid direction"),
-            };
-            let dist = dist.parse().unwrap();
-            (dir, dist)
+            }
         })
         .collect()
 }
 
-#[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
-struct Tail(i32, i32);
+#[derive(Copy, Clone, Hash, PartialEq, Eq)]
+struct Knot(i32, i32);
 
-#[derive(Copy, Clone)]
-enum Move {
-    Up,
-    Down,
-    Left,
-    Right,
-}
-
-impl Tail {
-    fn move_head(&mut self, m: Move, dist: i32) {
-        match m {
-            Move::Down => self.1 -= dist,
-            Move::Up => self.1 += dist,
-            Move::Left => self.0 -= dist,
-            Move::Right => self.0 += dist,
-        }
+impl Knot {
+    #[inline]
+    fn pull(&mut self, to: (i32, i32)) {
+        self.0 += to.0;
+        self.1 += to.1;
     }
 
-    fn move_tail(&mut self, head: Tail) -> bool {
+    #[inline]
+    fn follow(&mut self, head: Knot) -> bool {
         let (x, y) = (head.0 - self.0, head.1 - self.1);
         if x.abs() < 2 && y.abs() < 2 {
             return false;
